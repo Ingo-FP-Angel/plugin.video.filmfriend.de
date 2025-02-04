7b3bd7e0098c24839e635ff437fdd8df4fbdba1e
rantan-plan <101791743+rantan-plan@users.noreply.github.com>
2025-02-04 13:44:19 +0100

    Add option to filter for movie genres.

--- plugin.video.filmfriend.de/default.py
+++ plugin.video.filmfriend.de/default.py
@@ -12,6 +12,7 @@ class filmfriend(lm4):
 			'listMain': self.listMain,
 			'listVideos': self.listVideos,
 			'listWatchList': self.listWatchList,
+			'listGenres': self.listGenres,
 		})
 
 		self.searchModes = {
@@ -26,12 +27,21 @@ class filmfriend(lm4):
 		l = []
 		l.append({'metadata':{'name':self.translation(32030)}, 'type':'dir', 'params':{'mode':'listSearch', 'content':'videos',  'params':'?facets=Kind&facets=VideoKind&facets=Categories&facets=Genres&facets=GameSituations&facets=AgeRecommendation&facets=AudioLanguages&facets=AudioDescriptionLanguages&facets=SubtitleLanguages&facets=ClosedCaptionLanguages&kinds=Video&kinds=Series&videoKinds=Movie&&&&orderBy=ActiveSinceDateTime&sortDirection=Descending&skip=0&take=20'}})
 		l.append({'metadata':{'name':self.translation(32031)}, 'type':'dir', 'params':{'mode':'listSearch', 'content':'videos',  'params':'?facets=Kind&facets=VideoKind&facets=Categories&facets=Genres&facets=GameSituations&facets=AgeRecommendation&facets=AudioLanguages&facets=AudioDescriptionLanguages&facets=SubtitleLanguages&facets=ClosedCaptionLanguages&kinds=Video&videoKinds=Movie&&&&orderBy=MonthlyImpressionScore&sortDirection=Descending&skip=0&take=20'}})
+		l.append({'metadata':{'name': 'Genres'}, 'type':'dir', 'params':{'mode':'listGenres', 'content':'videos',  'params':'?totalCount=true&take=500&sortOrder=RecentlyAdded'}})
 		l.append({'metadata':{'name':self.translation(30600)}, 'type':'dir', 'params':{'mode':'listSearch', 'content':'tvshows', 'params':'?facets=Kind&facets=VideoKind&facets=Categories&facets=Genres&facets=GameSituations&facets=AgeRecommendation&facets=AudioLanguages&facets=AudioDescriptionLanguages&facets=SubtitleLanguages&facets=ClosedCaptionLanguages&kinds=Series&&languageIsoCode=EN&orderBy=EnglishOrder&sortDirection=Ascending&skip=0&take=500'}})
 		l.append({'metadata':{'name':self.translation(30601)}, 'type':'dir', 'params':{'mode':'listSearch', 'content':'movies',  'params':'?facets=Kind&facets=VideoKind&facets=Categories&facets=Genres&facets=GameSituations&facets=AgeRecommendation&facets=AudioLanguages&facets=AudioDescriptionLanguages&facets=SubtitleLanguages&facets=ClosedCaptionLanguages&kinds=Video&videoKinds=Movie&categories=d36cbed2-7569-4b94-9080-03ce79c2ecee&orderBy=EnglishOrder&sortDirection=Ascending&skip=0&take=500'}})
 		l.append({'metadata':{'name':self.translation(30602)}, 'type':'dir', 'params':{'mode':'listWatchList', 'content':'videos',  'params':'?totalCount=true&take=500&sortOrder=RecentlyAdded'}})
 		l.append({'metadata':{'name':self.translation(32139)}, 'params':{'mode':'libMediathekSearch', 'searchMode':'listVideoSearch'}, 'type':'dir'})
 		return {'items':l,'name':'root'}
 
+	def listGenres(self):
+		genre_names, genre_ids = jsonParser.parseGenres()
+		l = []
+		for genre_name, genre_id in zip(genre_names, genre_ids):
+			l.append({'metadata':{'name': genre_name}, 'type':'dir', 'params':{'mode':'listSearch', 'content':'videos',  'params': f'?facets=Kind&facets=VideoKind&facets=Categories&facets=Genres&facets=GameSituations&facets=AgeRecommendation&facets=AudioLanguages&facets=AudioDescriptionLanguages&facets=SubtitleLanguages&facets=ClosedCaptionLanguages&kinds=Video&kinds=Series&videoKinds=Movie&genres={genre_id}&&&orderBy=EnglishOrder&sortDirection=Ascending&skip=0&take=500'}})
+		
+		return {'items':l}
+    
 	def listSearch(self):
 		return jsonParser.parseSearch(self.params['params'],self.params['content'])
 
